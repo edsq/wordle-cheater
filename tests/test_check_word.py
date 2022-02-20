8145929628ee24bff25c414cdb96268b75743d1d
Edward Eskew <edward.eskew@wsu.edu>
2022-02-20 01:39:36 -0800

    Update tests of check_word to use new format for black letters

--- tests/test_check_word.py
+++ tests/test_check_word.py
@@ -4,7 +4,7 @@ from wordle_cheater.cheater import check_word
 
 # Wordle from 02-07-2022 (solution 'elder')
 # My guesses were 'beats', 'oiled', 'elder'
-blacks = ["b", "a", "t", "s", "o", "i"]
+blacks = [["b"], ["a"], ["t"], ["s"], ["o"], ["i"]]
 yellows = [[], ["e"], ["l"], [], ["d"]]
 greens = [None, None, None, "e", None]
 
@@ -84,7 +84,13 @@ def test_check_word_invalid_wordlist():
 def test_check_word_two_repeated_letters():
     # Test if check_word properly handles when two letters repeat
     # Guess: "array", answer: "aroma"
-    blacks = ["r", "y"]  # second r marked black as it only appears in answer once
+    blacks = [
+        [],
+        [],
+        ["r"],
+        [],
+        ["y"],
+    ]  # second r marked black as it only appears in answer once
     yellows = [
         [],
         [],
@@ -99,7 +105,7 @@ def test_check_word_two_repeated_letters():
 def test_check_word_green_and_black():
     # Test if check_word properly handles when a letter is marked both green and black
     # Guess: "berry", answer: "score"
-    blacks = ["b", "r", "y"]
+    blacks = [["b"], [], ["r"], [], ["y"]]
     yellows = [
         [],
         ["e"],
@@ -114,7 +120,7 @@ def test_check_word_green_and_black():
 def test_check_word_green_and_yellow():
     # Test if check_word properly handles when a letter is marked both green and yellow
     # Guess: "berry", answer: "roars"
-    blacks = ["b", "e", "y"]
+    blacks = [["b"], ["e"], [], [], ["y"]]
     yellows = [
         [],
         [],
@@ -129,7 +135,7 @@ def test_check_word_green_and_yellow():
 def test_check_word_yellow_and_black():
     # Test if check_word properly handles when a letter is marked both green and yellow
     # Guess: "array", answer: "leash"
-    blacks = ["a", "r", "r", "y"]
+    blacks = [["a"], ["r"], ["r"], [], ["y"]]
     yellows = [
         [],
         [],
@@ -144,12 +150,12 @@ def test_check_word_yellow_and_black():
 def test_check_word_green_yellow_black():
     # Test if check_word properly handles when a letter is marked all three colors
     # Guess: "sassy", answer: "shops"
-    blacks = ["a", "s", "y"]
+    blacks = [[], ["a"], [], ["s"], ["y"]]
     yellows = [
         [],
         [],
-        [],
         ["s"],
+        [],
         [],
     ]  # second a marked yellow as it appears in answer in a different position
     greens = ["s", None, None, None, None]
