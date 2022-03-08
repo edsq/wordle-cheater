5af54f7605eac5e0e936a69d3a7d3175169a35ff
Edward Eskew <edward.eskew@wsu.edu>
2022-03-07 20:30:12 -0800

    Add linting with flake8 session to noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,9 +2,22 @@
 # https://github.com/cjolowicz/nox-poetry
 from nox_poetry import session
 
+locations = ("src", "tests", "noxfile.py")
+
 
 @session(python=["3.7", "3.8", "3.9", "3.10"])
 def tests(session):
     """Run test suite for each supported python version."""
     session.install("pytest", "coverage[toml]", "pytest-cov", ".")
     session.run("pytest", "--cov")
+
+
+@session(python=["3.7", "3.8", "3.9", "3.10"])
+def lint(session):
+    if session.posargs:
+        args = session.posargs
+    else:
+        args = locations
+
+    session.install("flake8", "flake8-black")
+    session.run("flake8", *args)
